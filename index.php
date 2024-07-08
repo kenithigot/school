e1e3b44b8378591b6ed1b8ddfc2e3a8a797363fd
kennithigot <kennithigot1@gmail.com>
2024-07-08 12:01:19 +0800

    school duplication ui

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>La Salle University - Ozamiz</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style.css" rel="stylesheet">
+    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <div class="nav-bg">
+            <nav class="navbar navbar-expand-lg fixed-top nav-bg">
+                <div class="container">
+                    <a class="navbar-brand text-light fs-3 fw-bold" href="#">La Salle University </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+                        <div class="d-flex px-2 fs-6 text-uppercase fw-bold" role="search">
+                            <li class="nav-item navbar-nav">
+                                <a class="nav-link active text-light " aria-current="page" href="#">Home</a>
+                            </li>       
+                        </div>
+                        <div class="d-flex px-2 fs-6 fw-bold" role="search"> 
+                            <li class="nav-item dropdown navbar-nav nav-hover">
+                                <a class="nav-link dropdown-toggle text-light text-uppercase" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Academics</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Tertiary Education</a></li>
+                                    <li><a class="dropdown-item" href="#">Class Schedules</a></li>
+                                </ul>
+                            </li>
+                        </div>
+                        <div class="d-flex px-2 fs-6 text-uppercase fw-bold" role="search">
+                            <li class="nav-item navbar-nav">
+                                <a class="nav-link active text-light" aria-current="page" href="#">Administrator</a>
+                            </li>       
+                        </div>
+                        <div class="d-flex px-2 fs-6 text-uppercase fw-bold" role="search">
+                            <li class="nav-item navbar-nav">
+                                <a class="nav-link active text-light" aria-current="page" href="#">Contact</a>
+                            </li>       
+                        </div>
+                        <div class="d-flex px-2 fs-6 fw-bold text-adjust" role="search"> 
+                            <li class="nav-item dropdown navbar-nav nav-hover">
+                                <a class="nav-link dropdown-toggle text-light text-uppercase" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">More</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Registrar</a></li>
+                                    <li><a class="dropdown-item" href="#">Procurement</a></li>
+                                    <li><a class="dropdown-item" href="#">Events</a></li>
+                                    <li><a class="dropdown-item" href="#">About</a></li>
+                                    <li><a class="dropdown-item" href="#">Human Resource</a></li>
+                                </ul>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div class="bg-color pad-top">   
+                <div class="carousel-img">
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" checked/>
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <main id="carousel">
+                        <div class="item"><img src="resources/imgs/pic1.jpg" alt=""></div>
+                        <div class="item"><img src="resources/imgs/pic2.jpg" alt="asdas"></div>
+                        <div class="item"><img src="resources/imgs/pic3.jpg" alt="asdas"></div>
+                        <div class="item"><img src="resources/imgs/pic4.jpg" alt="asdas"></div>
+                        <div class="item"><img src="resources/imgs/pic5.jpg" alt="asdas"></div>
+                    </main>
+                </div>
+            </div>
+            <div class="bg-light">
+                <div class="container-fluid text-center fs-5 text-uppercase fw-bold pt-2 px-4">
+                    <div class="row border-nav justify-content-center">
+                        <div class="col col-md-2 link-hov">
+                            <a class="nav-link p-3" href="#"><i class="bi bi-file-earmark-text-fill"></i>&nbspEnrollment</a>
+                        </div>
+                        <div class="col-6 col-md-2 link-hov">
+                            <a class="nav-link p-1 pt-3" href="#"><i class="bi bi-card-text d-inline justify-content-center"></i>&nbspScholarships</a>
+                        </div>
+                        <div class="col-6 col-md-2 link-hov">
+                            <a class="nav-link p-3" href="#"><i class="bi bi-person-square"></i>&nbspMY.LSU</a>
+                        </div>
+                        <div class="col-6 col-md-2 link-hov">
+                            <a class="nav-link p-3" href="#"><i class="bi bi-journals"></i>&nbspLibrary</a>
+                        </div>
+                        <div class="col-6 col-md-2 link-hov">
+                            <a class="nav-link p-3" href="#"><i class="bi bi-arrow-up-right-square"></i>&nbspCanvas</a>
+                        </div>
+                        <div class="col-6 d-block d-md-none link-hov">
+                            <a class="nav-link p-3" href="#"><i class="bi bi-exclamation-circle-fill"></i>&nbspFAQS</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid pt-4">
+                    <div class="row align-items-md-stretch">
+                        <div class="col-md-5">
+                            <div class="h-100 p-5 rounded-3 border bg-body-tertiary">
+                                <h3 class="text-center">La Salle University - Ozamiz</h3>
+                                <h5 class="text-center"><i>Advances and Linkages</i></h5>
+                                <img src="resources/imgs/pic3.jpg" class="card-img-top" alt="...">
+                                <div class="btn-card mt-3">
+                                    <button class="btn btn-success text-uppercase" type="button">Learn More</button>
+                                </div> 
+                            </div>    
+                        </div>
+                        <div class="col-md-7">
+                            <div class="h-100 p-3 border bg-color rounded-3">
+                                <h4 class="text-center fw-bold text-light">University Events and Activities</h4>
+                                <div class="d-flex flex-column flex-md-row gap-4 align-items-center justify-content-center mt-3">
+                                    <div class="list-group">
+                                        <div href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                                            <img src="resources/imgs/check.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                                <div>
+                                                <h6 class="mb-0">LSU University Student Government</h6>
+                                                <p class="mb-0 opacity-75">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                </div>
+                                                <small class="opacity-50 text-nowrap">now</small>
+                                            </div>
+                                        </div>
+                                        <div href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                                            <img src="resources/imgs/check.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                                <div>
+                                                    <h6 class="mb-0">LSU Sports and Wellness Center</h6>
+                                                    <p class="mb-0 opacity-75">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                </div>
+                                                <small class="opacity-50 text-nowrap">3d</small>
+                                            </div>
+                                        </div>
+                                        <div href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                                            <img src="resources/imgs/check.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                                <div>
+                                                    <h6 class="mb-0">Arts and Culture Center</h6>
+                                                    <p class="mb-0 opacity-75">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                </div>
+                                                <small class="opacity-50 text-nowrap">3d</small>
+                                            </div>
+                                        </div>
+                                        <div href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                                            <img src="resources/imgs/check.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                                <div>
+                                                    <h6 class="mb-0">Institutional Events</h6>
+                                                    <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
+                                                </div>
+                                                <small class="opacity-50 text-nowrap">1w</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="container text-center bg-body-tertiary">
+                                    <div class="row mt-3">
+                                        <div class="col-2 border-preview fs-1">
+                                            <i class="bi bi-facebook"></i>
+                                        </div>
+                                        <div class="col-2 border-preview fs-1">
+                                            <i class="bi bi-messenger"></i>
+                                        </div>
+                                        <div class="col-2 border-preview fs-1">
+                                            <i class="bi bi-twitter"></i>
+                                        </div>
+                                        <div class="col-2 border-preview fs-1">
+                                            <i class="bi bi-whatsapp"></i>
+                                        </div>
+                                        <div class="col-2 border-preview fs-1">
+                                            <i class="bi bi-bootstrap-fill"></i>
+                                        </div>
+                                        <div class="col-2 border-preview fs-1">
+                                            <i class="bi bi-amazon"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion" id="accordionPanelsStayOpenExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                    <div class="accord-content">
+                                        <i class="bi bi-lightbulb-fill"></i><i>LSU Official Advisory</i>
+                                    </div>
+                                    <div class="accord-content">
+                                        <i class="bi bi-newspaper"></i></i><i>LSU Official Advisory</i>
+                                    </div>
+                                </button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+                                    <div class="accordion-body">
+                                        <strong>Lorem ipsum dolor sit amet,</strong> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                            Ullamcorper eget nulla facilisi etiam dignissim. Massa tincidunt dui ut ornare lectus sit amet est placerat. 
+                                            Nisi porta lorem mollis aliquam. Arcu risus quis varius quam quisque id diam vel. Id neque aliquam vestibulum morbi blandit cursus. 
+                                            In nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. 
+                                            Felis bibendum ut tristique et egestas quis ipsum suspendisse. Semper feugiat nibh sed pulvinar proin gravida hendrerit. 
+                                            Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Tempus urna et pharetra pharetra.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container bg-color">
+                            <footer class=" my-5">
+                                <div class="px-4 my-3 text-center">
+                                    <h1 class="fs-3 text-light mb-3">La Salle University</h1>
+                                    <h1 class="fs-2 fw-bold text-light text-uppercase">Reshaping Futures</h1>
+                                    <div class="col-lg-6 mx-auto">
+                                        <p class="fs-6 mb-0 text-light">© 2024 La Salle University - Ozamiz</p>
+                                        <p class="fs-6 mb-4 text-light">La Salle St. Barangay Aguada Ozamiz City, Misamis Occidental, Philippines</p>
+                                    </div>
+                                </div>
+                                <ul class="nav justify-content-center">
+                                    <li class="nav-item"><a href="#" class="nav-link text-light p-0 px-2 border-end">Privacy Policy</a></li>
+                                    <li class="nav-item"><a href="#" class="nav-link text-light p-0 px-2 border-end">Downloads</a></li>
+                                    <li class="nav-item"><a href="#" class="nav-link text-light p-0 px-2 border-end">Contact Us</a></li>
+                                    <li class="nav-item"><a href="#" class="nav-link text-light p-0 px-2 border-end">Procurement Admin</a></li>
+                                    <li class="nav-item"><a href="#" class="nav-link text-light p-0 px-2 ">Social Medias</a></li>
+                                </ul>
+                                <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+                            </footer>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
